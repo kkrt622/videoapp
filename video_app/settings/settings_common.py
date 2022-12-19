8171f98de66c8bd3b8f0697b26a8c1f0a1766ba7
guro-Ishiguro <ryu32349581@gmail.com>
2022-12-19 16:09:38 +0900

    ユーザーモデルの作成

--- video_app/settings/settings_common.py
+++ video_app/settings/settings_common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,3 +147,5 @@ ACCOUNT_LOGOUT_ON_GET = True
 ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 
 DEFAUTL_FROM_EMAIL = "admin@example.com"
+
+MEDIA_URL = "/media/"
--- video_app/settings/settings_dev.py
+++ video_app/settings/settings_dev.py
@@ -7,6 +7,8 @@ ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 try:
     from .settings_local import *  # noqa: F401,F403
 except ImportError:
