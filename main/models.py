1a69251ed2159b7f2ad18a8a03a71b35ff458b1f
guro-Ishiguro <ryu32349581@gmail.com>
2023-02-06 16:34:47 +0900

    null=Trueの不必要な部分の削除

--- main/models.py
+++ main/models.py
@@ -10,7 +10,7 @@ import uuid
 class User(AbstractUser):
     email = models.EmailField("メールアドレス", unique=True)
     username = models.CharField("ユーザ名", max_length=50, default="ゲスト", unique=False)
-    profile = models.TextField("プロフィール", max_length=500, null=True)
+    profile = models.TextField("プロフィール", max_length=500)
     icon = models.ImageField("アイコン", upload_to="icon/", blank=True)
     follow = models.ManyToManyField("User", related_name="followed", symmetrical=False)
 
